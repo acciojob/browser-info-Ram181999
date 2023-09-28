233ed5a410a6b07b7a53978167becbd11f3d6561
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-28 20:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+ // Get browser name and version from the navigator object
+        const browserName = navigator.appName;
+        const browserVersion = navigator.appVersion;
+
+        // Create a message with the browser information
+        const message = "You are using " + browserName + " version " + browserVersion;
+
+        // Find the browser-info div and update its content
+        const browserInfoDiv = document.getElementById("browser-info");
+        browserInfoDiv.textContent = message;
